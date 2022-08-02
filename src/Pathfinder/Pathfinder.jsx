4713dc8e06456eadf65752769cff55af0c41f79b
Hunter Hansen <hunterhansen50@gmail.com>
2022-08-02 12:16:57 -0600

    implement dijkstras algorithm

--- src/Pathfinder/Pathfinder.jsx
+++ src/Pathfinder/Pathfinder.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Node from "./node/Node";
+import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
+
 import "./Pathfinder.css";
 
 import Container from "react-bootstrap/Container";
@@ -26,56 +28,116 @@ export default class Pathfinder extends Component {
       grid: [],
       startNode: { row: START_NODE_ROW, col: START_NODE_COL },
       finishNode: { row: FINISH_NODE_ROW, col: FINISH_NODE_COL },
-      algorithm: "BFS",
+      algorithm: "Dijkstra",
       object: "Wall"
     };
   }
 
   componentDidMount() {
     const grid = initGrid();
-    this.setState({ grid });
+    this.setState({ grid: grid });
+  }
+
+  resetGrid() {
+    const grid = initGrid();
+    resetVisitedNodes(grid);
+    this.setState({
+      mouseIsPressed: false,
+      grid: grid,
+      startNode: { row: START_NODE_ROW, col: START_NODE_COL },
+      finishNode: { row: FINISH_NODE_ROW, col: FINISH_NODE_COL },
+      algorithm: "Dijkstra",
+      object: "Wall"
+    });
   }
 
   handleMouseDown(row, col) {
     this.setState({ mouseIsPressed: true });
-    this.handleMouseOver(row, col);
-  }
-
-  handleMouseOver(row, col) {
-    if (!this.state.mouseIsPressed) {
-      return
-    }
-
-    const { grid } = this.state;
-    const newGrid = grid.slice();
-    const newNode = newGrid[row][col];
-
     if (this.state.object === "Wall") {
-      newNode.isWall = true;
+      const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+      this.setState({ grid: newGrid });
     }
-
-    if (this.state.object === "Start") {
-      const oldStart = this.state.startNode;
-      newGrid[oldStart.row][oldStart.col].isStart = false;
-
-      newNode.isStart = true;
-      this.setState({ startNode: {row, col} });
+    else if (this.state.object === "Start") {
+      const newNode = { row, col };
+      const newGrid = getNewGridWithStartToggled(this.state.grid, newNode, this.state.startNode);
+      this.setState({ grid: newGrid, startNode: { row, col } });
+    } else if (this.state.object === "End") {
+      const newNode = { row, col };
+      const newGrid = getNewGridWithFinishToggled(this.state.grid, newNode, this.state.finishNode);
+      this.setState({ grid: newGrid, finishNode: { row, col } });
     }
-
-    if (this.state.object === "End") {
-      const oldFinish = this.state.finishNode;
-      newGrid[oldFinish.row][oldFinish.col].isFinish = false;
-
-      newNode.isFinish = true;
-      this.setState({ finishNode: {row, col} });
-    }
-
-    this.setState({ newGrid });
   }
 
-  handleMouseUp(row, col) {
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    
+    if (this.state.object === "Wall") {
+      const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+      this.setState({ grid: newGrid });
+    }
+    else if (this.state.object === "Start") {
+      const newNode = { row, col };
+      const newGrid = getNewGridWithStartToggled(this.state.grid, newNode, this.state.startNode);
+      this.setState({ grid: newGrid, startNode: { row, col } });
+    }
+    else if (this.state.object === "End") {
+      const newNode = { row, col };
+      const newGrid = getNewGridWithFinishToggled(this.state.grid, newNode, this.state.finishNode);
+      this.setState({ grid: newGrid, finishNode: { row, col } });
+    }
+  }
+
+  handleMouseUp() {
     this.setState({ mouseIsPressed: false });
-    this.handleMouseOver(row, col);
+  }
+
+  handleVisualize() {
+    const { grid, startNode, finishNode, algorithm } = this.state;
+    const start = grid[startNode.row][startNode.col];
+    const finish = grid[finishNode.row][finishNode.col];
+
+
+    if (algorithm === "BFS") {
+      this.visualizeBFS(grid, start, finish);
+    } else if (algorithm === "DFS") {
+      this.visualizeDFS(grid, start, finish);
+    } else if (algorithm === "Dijkstra") {
+      this.visualizeDijkstra(grid, start, finish);
+    } else if (algorithm === "A*") {
+      this.visualizeAStar(grid, start, finish);
+    }
+  }
+
+  visualizeDijkstra(grid, startNode, finishNode) {
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+  }
+
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-visited";
+      }, 10 * i);
+    }
+  }
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-shortest-path";
+      }, 50 * i);
+    }
   }
 
   render() {
@@ -110,8 +172,8 @@ export default class Pathfinder extends Component {
           <Col>
             <ButtonToolbar>
               <DropdownButton
-                variant="light"
-                className="mx-2"
+                variant="outline-dark"
+                className="mx-1"
                 id="algorithm-dropdown"
                 title={this.state.algorithm}
               >
@@ -128,7 +190,10 @@ export default class Pathfinder extends Component {
                   A*
                 </Dropdown.Item>
               </DropdownButton>
-              <Button variant="dark" onClick={() => console.log(this.state.algorithm)}>
+              <Button variant="dark" className="mx-1" onClick={() => this.resetGrid()}>
+                Reset
+              </Button>
+              <Button variant="success" className="mx-1" onClick={() => this.handleVisualize()}>
                 Visualize
               </Button>
             </ButtonToolbar>
@@ -153,7 +218,7 @@ export default class Pathfinder extends Component {
                           onMouseDown={() => this.handleMouseDown(rowIdx, nodeIdx)}
                           onMouseUp={() => this.handleMouseUp(rowIdx, nodeIdx)}
                           onMouseOver={() =>
-                            this.handleMouseOver(rowIdx, nodeIdx)
+                            this.handleMouseEnter(rowIdx, nodeIdx)
                           }
                         />
                       );
@@ -184,12 +249,89 @@ function initGrid() {
   return grid;
 }
 
+function resetVisitedNodes() {
+  for (let row = 0; row < NUM_ROWS; row++) {
+    for (let col = 0; col < NUM_COLS; col++) {
+      if (row === START_NODE_ROW && col === START_NODE_COL) {
+        document.getElementById(`node-${row}-${col}`).className =
+        "node node-start";
+        continue;
+      }
+
+      if (row === FINISH_NODE_ROW && col === FINISH_NODE_COL) {
+        document.getElementById(`node-${row}-${col}`).className =
+        "node node-finish";
+        continue;
+      }
+
+      document.getElementById(`node-${row}-${col}`).className =
+      "node";
+    }
+  }
+}
+
 function createNode(row, col) {
   return {
     row,
     col,
     isStart: row === START_NODE_ROW && col === START_NODE_COL,
     isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    isVisited: false,
+    distance: Infinity,
     isWall: false,
+    previousNode: null
   };
+}
+
+function getNewGridWithWallToggled(grid, row, col) {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+}
+
+function getNewGridWithStartToggled(grid, newNode, oldNode) {
+  const newGrid = grid.slice();
+
+  const oldStart = newGrid[oldNode.row][oldNode.col];
+  const changedOld = {
+    ...oldStart,
+    isStart: false,
+  }
+  newGrid[oldNode.row][oldNode.col] = changedOld;
+
+  const { row, col } = newNode;
+  const newStart = newGrid[row][col];
+  const changedNew = {
+    ...newStart,
+    isStart: true,
+  };
+  newGrid[row][col] = changedNew;
+
+  return newGrid;
+}
+
+function getNewGridWithFinishToggled(grid, newNode, oldNode) {
+  const newGrid = grid.slice();
+
+  const oldFinish = newGrid[oldNode.row][oldNode.col];
+  const changedOld = {
+    ...oldFinish,
+    isFinish: false,
+  }
+  newGrid[oldNode.row][oldNode.col] = changedOld;
+
+  const { row, col } = newNode;
+  const newFinish = newGrid[row][col];
+  const changedNew = {
+    ...newFinish,
+    isFinish: true,
+  };
+  newGrid[row][col] = changedNew;
+
+  return newGrid;
 }
--- src/Pathfinder/node/Node.jsx
+++ src/Pathfinder/node/Node.jsx
@@ -4,7 +4,7 @@ import "./Node.css";
 export default class Node extends Component {
   render() {
     const { row, col, isStart, isFinish, isWall, onMouseDown, onMouseOver, onMouseUp } = this.props;
-    const cssClass = isStart ? "border border-danger bg-danger" : isFinish ? "border border-success bg-success": isWall ? "border border-secondary bg-secondary" : "border border-secondary";
+    const cssClass = isStart ? "node-start" : isFinish ? "node-finish": isWall ? "node-wall" : "";
 
     return (
       <div
