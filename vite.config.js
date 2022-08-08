135e23e12f1c9e8dcc2b858666b843110ad35362
Hunter Hansen <hunterhansen50@gmail.com>
2022-08-08 09:25:53 -0600

    Revert "update configs"
    This reverts commit 7319b656fceaae1a8b562902c4c1ef8ac6d4e830.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/visualizer/', // This is the base path for your website.
   plugins: [react()]
 })
